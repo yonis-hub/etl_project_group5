4b97a7e3ac86d30e7ab60ebaf7db3aa8a3be474d
Yonis Hassan <hyonis@fb.com>
2021-03-27 09:50:06 -0700

    add the df into db

--- queries.sql
+++ queries.sql
@@ -1,21 +1,20 @@
 -- Create tables for raw data to be loaded into
 CREATE TABLE world_happiness (
-id INT PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 country TEXT,
 year INT,
 happiness_rank INT
 );
 
 CREATE TABLE annual_work_hours (
-id INT PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 country TEXT,
 year INT,
 avg_work_hours INT
 );
 
+select * 
+from world_happiness;
 
--- -- Joins tables
--- SELECT customer_name.id, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
--- FROM customer_name
--- JOIN customer_location
--- ON customer_name.id = customer_location.id;
+select * 
+from annual_work_hours;
